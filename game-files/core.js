3c7b77118833142c12de9b1dc8ba9b5c10ae3758
pmenzel2007 <pmenzel2007@gmail.com>
2025-02-13 11:14:20 +0100

    Added Movement to player

--- game-files/core.js
+++ game-files/core.js
@@ -3,17 +3,18 @@ let ctx;
 let player;
 
 function onBodyLoad() {
-    alert("a");
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
 
     player = new Player(123, 123);
+
     gameLoop();
 }
 
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    player.move();
     player.draw(ctx);
 
     requestAnimationFrame(gameLoop);
--- game-files/player.js
+++ game-files/player.js
@@ -1,11 +1,27 @@
 class Player extends GameObject {
     constructor(x, y) {
         super(x, y, 32, 32);
-        this.speed = 4;
+        this.speed = 3;
+        this.movement = { up: false, down: false, left: false, right: false };
+
+        document.addEventListener("keydown", (event) => this.handleInput(event, true));
+        document.addEventListener("keyup", (event) => this.handleInput(event, false));
     }
 
-    move(dirX, dirY) {
-        this.x = this.x + dirX;
-        this.y = this.y + dirY;
+    handleInput(event, down) {
+        switch (event.code) {
+            case "ArrowUp": this.movement.up = down; break;
+            case "ArrowDown": this.movement.down = down; break;
+            case "ArrowRight": this.movement.right = down; break;
+            case "ArrowLeft": this.movement.left = down; break;
+        }
+        console.log(this.movement);
+    }
+
+    move() {
+        if (this.movement.up) this.y -= this.speed;
+        if (this.movement.down) this.y += this.speed;
+        if (this.movement.left) this.x -= this.speed;
+        if (this.movement.right) this.x += this.speed;
     }
 }
